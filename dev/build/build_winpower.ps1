d1274dfcf7752639faaa4d2ad49196890ad76ea2
abdul ahad <csabdulahad@gmail.com>
2023-06-05 01:04:50 +0100

    v4.0.0 whelp built-in command added & more optimization

--- dev/build/build_winpower.ps1
+++ dev/build/build_winpower.ps1
@@ -23,6 +23,19 @@ foreach ($file in $files) {
     $path = 'cmd/' + $file.Name.Replace('ps1', 'cmd');
     $cmdCode = '
 @echo off
+
+powershell -Command "if ((Get-ExecutionPolicy) -ne \"RemoteSigned\") { exit 1; }"
+if %errorlevel% equ 1 (
+    echo WinPower does not have enough permission to run.
+
+    setlocal enabledelayedexpansion
+    set /p "input=Do you want to run wp setup? (y/n): "
+    if "!input!" equ "y" (
+        call c:/winpower/cmd/wp_setup.cmd
+    )
+    exit
+)
+
 set "a=%*"
 powershell -File "c:/winpower/ps/wp.ps1" "' + $file.Name.Replace('.ps1', '') + '" %a%';
 
--- winpower/cmd/ip.cmd
+++ winpower/cmd/ip.cmd
@@ -1,4 +1,17 @@
 
 @echo off
+
+powershell -Command "if ((Get-ExecutionPolicy) -ne \"RemoteSigned\") { exit 1; }"
+if %errorlevel% equ 1 (
+    echo WinPower does not have enough permission to run.
+
+    setlocal enabledelayedexpansion
+    set /p "input=Do you want to run wp setup? (y/n): "
+    if "!input!" equ "y" (
+        call c:/winpower/cmd/wp_setup.cmd
+    )
+    exit
+)
+
 set "a=%*"
 powershell -File "c:/winpower/ps/wp.ps1" "ip" %a%
